988b835339392a718ea303a6a487e6fbedeeaaf3
Ashish Mishra <69194675+ashish3010@users.noreply.github.com>
2023-11-14 15:10:11 +0530

    Update index.js

--- remove duplicates/index.js
+++ remove duplicates/index.js
@@ -5,6 +5,20 @@ const removeDuplicates = (list) => {
   return [...new Set(list)]
 }
 
+// using object
+const removeDuplicates = (list) => {
+  const obj = {}
+  const res = []
+
+  for (let i = 0; i < list.length; i++) {
+    if (obj[list[i]] !== undefined) {
+      continue
+    }
+    obj[list[i]] = true
+    res.push(list[i])
+  }
+  return res
+}
 
 
 console.log(removeDuplicates(a))
