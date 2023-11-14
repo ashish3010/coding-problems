b8776f8963a8b5579eccca0e541a435957854183
Ashish Mishra <69194675+ashish3010@users.noreply.github.com>
2023-11-14 14:55:13 +0530

    Update index.js

--- move all zeros at end/index.js
+++ move all zeros at end/index.js
@@ -1,10 +1,10 @@
-const a = [0, 2, 0, 4, 2, 5, 0, 2, 0, 7, 0, 35, 7, 9, 3, 4, 6]
+// const a = [0, 2, 0, 4, 2, 5, 0, 2, 0, 7, 0, 35, 7, 9, 3, 4, 6]
 
 const moveZeroToEnd = (list) => {
   const zero = []
   const nonZero = []
 
-  for (let i = 0; i < a.length; i++) {
+  for (let i = 0; i < list.length; i++) {
     if (list[i] === 0) {
       zero.push(list[i])
       continue
@@ -15,3 +15,4 @@ const moveZeroToEnd = (list) => {
   return [...nonZero, ...zero]
 }
 
+// console.log(moveZeroToEnd(a))
